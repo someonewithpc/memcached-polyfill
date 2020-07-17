326a44a1ce5f936b25aa69b5c9b82a7cc8e565b4
Hugo Sales <hugo@fc.up.pt>
2020-07-17 22:30:37 +0000

    Only defined classes if they don't exist

--- src/Memcached.php
+++ src/Memcached.php
@@ -1,9 +1,11 @@
 <?php
 
-class Memcached
-{
-    public function __construct()
+if (!class_exists('Memcached')) {
+    class Memcached
     {
-        throw new Exception("This polyfill doesn't actually implement anything, it's purpose is to allow for optional use in the symfony framework");
+        public function __construct()
+        {
+            throw new Exception("This polyfill doesn't actually implement anything, it's purpose is to allow for optional use in the symfony framework");
+        }
     }
 }
